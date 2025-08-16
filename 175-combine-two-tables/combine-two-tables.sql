600e927e370b07e7992e2c720b1fa6b2c42ac3ba
KhamankarOm4 <khamankaros@rknec.edu>
2025-08-16 09:56:43 +0530

    Time: 452 ms (80.40%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 SELECT Distinct a.lastName , a.firstName ,b.city ,b.state FROM person a LEFT JOIN address b ON a.personId = b.personId;
