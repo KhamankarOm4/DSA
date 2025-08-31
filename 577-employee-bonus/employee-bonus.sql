a94736d5f149e3630aa2671acb8f42a36df1369a
KhamankarOm4 <khamankaros@rknec.edu>
2025-08-31 07:48:38 +0530

    Time: 898 ms (82.24%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,2 +1,2 @@
-/* Write your PL/SQL query statement below */
-SELECT e.name , b.bonus from employee e LEFT JOIN  Bonus b ON b.empId=e.empId WHERE bonus<1000 or bonus IS NULL;
+# Write your MySQL query statement below
+SELECT e.name , b.bonus from employee e LEFT JOIN Bonus b ON e.empId = b.empId WHERE b.bonus < 1000 or b.bonus is null;
