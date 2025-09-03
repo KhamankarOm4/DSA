573e019abf622ea16dabd80859cb8e0968a2e02c
KhamankarOm4 <khamankaros@rknec.edu>
2025-09-03 20:52:19 +0530

    Time: 1670 ms (85.71%) | Memory: 13 MB (0.00%) - LeetSync

---
+++ 3277-find-the-number-of-ways-to-place-people-ii/find-the-number-of-ways-to-place-people-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def numberOfPairs(self, points):
+        points.sort(key=lambda p: (p[0], -p[1]))
+        n = len(points)
+        ans = 0
+        for i in range(n):
+            yi = points[i][1]
+            maxY = float("-inf")
+            for j in range(i+1, n):
+                yj = points[j][1]
+                if yj <= yi and yj > maxY:
+                    ans += 1
+                    maxY = yj
+        return ans
