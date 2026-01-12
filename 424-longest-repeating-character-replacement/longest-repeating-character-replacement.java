ee3e911574b33246e4ced0a1b4ed666131505602
KhamankarOm4 <khamankaros@rknec.edu>
2026-01-12 08:24:26 +0530

    Time: 12 ms (48.62%) | Memory: 46.1 MB (82.48%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int[] freq = new int[26];
+        int mostFreqLetter = 0;
+        int left = 0;
+        int max = 0;
+
+        for(int right = 0;right <s.length();right++){
+            freq[s.charAt(right)-'A']++;
+            mostFreqLetter = Math.max(mostFreqLetter,freq[s.charAt(right)-'A']);
+
+            int letterToChange = (right-left+1)-mostFreqLetter;
+            if(letterToChange>k){
+                freq[s.charAt(left)-'A']--;
+                left++;
+            }
+
+            max =Math.max(max,right-left+1);
+        }
+
+        return max;
+        
+    }
+}
