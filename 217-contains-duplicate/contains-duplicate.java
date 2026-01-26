881c942237760c61918db458727dc865fc520ca6
KhamankarOm4 <khamankaros@rknec.edu>
2026-01-26 18:02:21 +0530

    Time: 15 ms (64.82%) | Memory: 97.8 MB (11.91%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -3,11 +3,11 @@ class Solution {
         Map<Integer,Integer> map = new HashMap<>();
 
         for(int i=0;i<nums.length;i++){
+           if(map.containsKey(nums[i])){
+            return true;
+           }
             map.put(nums[i],i);
 
-        }
-        if(nums.length>map.size()){
-            return true;
         }
         return false;
     }
