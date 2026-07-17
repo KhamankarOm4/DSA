1abc056c1d5ee3c7272f2aa288eed02c6aa20304
Om Khamankar <khamankaros@rknec.edu>
2026-07-17 08:40:05 +0530

    Time: 2 ms (96.28%) | Memory: 48.6 MB (51.39%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,22 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
         int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
         for(int i =0;i<=n;i++){
-          recur(dp,i);  
+            dp[i] = dp[i>>1]+(i&1);
         }
         return dp;
-    }
-
-    public int recur(int[] dp , int n){
-       if(n == 0){
-        dp[0] = 0;
-        return dp[0];
-       }
-       if(dp[n]!=-1){
-        return dp[n];
-       }
-       dp[n] = recur(dp,n>>1) + (n&1);
-       return dp[n];
     }
 }
