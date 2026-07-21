0291cdcb8ce2a387fd3bb36b42a62feb16bf82c3
Om Khamankar <khamankaros@rknec.edu>
2026-07-21 18:07:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (93.54%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,24 +1,15 @@
 class Solution {
     public int maxProfit(int[] prices) {
-       
-        int n = prices.length;
+       int profit = 0;
 
-        int[][] dp = new int[n + 1][2];
-
-        for(int i = n - 1; i >= 0; i--) {
-
-            dp[i][1] = Math.max(
-                    -prices[i] + dp[i + 1][0],
-                    dp[i + 1][1]
-            );
-
-            dp[i][0] = Math.max(
-                    prices[i] + dp[i + 1][1],
-                    dp[i + 1][0]
-            );
+        // Sum all positive adjacent differences.
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
         }
 
-        return dp[0][1];
+        return profit;
     }
 
     
