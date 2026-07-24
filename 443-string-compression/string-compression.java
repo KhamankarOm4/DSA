7380dc096e600289c1aa9262ea6172b88e527594
Om Khamankar <khamankaros@rknec.edu>
2026-07-24 23:47:09 +0530

    Time: 1 ms (99.89%) | Memory: 45.4 MB (48.85%) - LeetSync

---
+++ 443-string-compression/string-compression.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int idx = 0;
+        for(int i=0;i<n;i++){
+            char ch = chars[i];
+            int count = 0;
+            while(i<n && chars[i]==ch){
+                count++;
+                i++;
+            }
+            if(count==1){
+                chars[idx++] = ch;
+            }else{
+                chars[idx++] = ch;
+                String s= Integer.toString(count);
+                for(char c:s.toCharArray()){
+                    chars[idx++] = c;
+                }
+            }
+            i--;
+        }
+        return idx;
+    }
+}
