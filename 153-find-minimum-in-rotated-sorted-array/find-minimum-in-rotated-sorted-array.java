3a965a23916afad8ae2e9bbe82f8c3c60d5495de
Om Khamankar <khamankaros@rknec.edu>
2026-07-26 23:18:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (98.68%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -6,7 +6,7 @@ class Solution {
         while (left < right) {
             int mid = left + (right - left) / 2;
 
-            if (nums[mid] < nums[right]) {
+            if (nums[mid] <= nums[right]) {
                 right = mid;
             } else {
                 left = mid + 1;
