345d233e6271280a58235697ff9d8a75dc428691
Om Khamankar <khamankaros@rknec.edu>
2026-09-05 18:17:06 +0530

    Time: 11 ms (62.08%) | Memory: 57.2 MB (29.17%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,18 +1,20 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
+        int[][] dp = new int[n][amount+1];;
+        for(int i=0;i<n;i++){
+            dp[i][0] = 1;
+        }
 
         for(int a=0;a<=amount;a++){
             if(a%coins[0]==0){
                 dp[0][a] = 1;
             }else{
-                dp[0][a] = 0;
+                dp[0][a] =  0;
             }
         }
-
         for(int i=1;i<n;i++){
-            for(int a=0;a<=amount;a++){
+            for(int a=1;a<=amount;a++){
                 int pick = 0;
                 if(a>=coins[i]){
                     pick = dp[i][a-coins[i]];
@@ -20,10 +22,6 @@ class Solution {
                 int nopick = dp[i-1][a];
                 dp[i][a] = pick + nopick;
             }
-        }
-
-        if(dp[n-1][amount] == (int)(1e9)){
-            return -1;
         }
         return dp[n-1][amount];
     }
