efadeba7110221dcbc32537cfe609abfd5c39665
Om Khamankar <khamankaros@rknec.edu>
2026-09-20 01:09:47 +0530

    Time: 8 ms (34.44%) | Memory: 50.1 MB (21.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -10,15 +10,7 @@ class Solution {
         }
         Arrays.sort(ele);
         String key = String.valueOf(ele);
-        if(!map.containsKey(key)){
-            List<String> arr = new ArrayList<>();
-            arr.add(s);
-            map.put(key,arr);
-        }else{
-            List<String> arr = map.get(key);
-            arr.add(s);
-            map.put(key,arr);
-        }
+        map.computeIfAbsent(key,k->new ArrayList<>()).add(s);
      }
 
      for(String key:map.keySet()){
